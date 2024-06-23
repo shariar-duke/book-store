366b3a829b2ebb081c9ddd8356847efb24f83a26
shariar-duke <shariarmahmud225@gmail.com>
2024-06-23 08:24:58 +0600

    Book Map

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
-
+import React from "react";
+import { useState } from "react";
+import { getBookList } from "../data/book-data.js";
+import Book from "./Book.jsx";
 export default function BookList() {
+  const books = getBookList();
+  const [bookList, setBookList] = useState(books);
+
+  console.log("The BookList are", bookList);
   return (
-    <div>BookList</div>
-  )
+    <div className="container mx-auto grid grid-cols-1 gap-8 max-w-7xl md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {bookList.map((b, index) => (
+        <Book key={b.id} />
+      ))}
+    </div>
+  );
 }
