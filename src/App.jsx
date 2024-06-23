ffbb1244e1675f05141b812aa65a46912998591b
shariar-duke <shariarmahmud225@gmail.com>
2024-06-23 08:25:37 +0600

    background image added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import Nav from "./components/Nav";
 import Hero from "./components/Hero";
 import BookList from "./components/BookList";
+import bg from "./assets/bookBackground.jpg"
 export default function App() {
   return (
-    <div>
-   <div className="flex flex-col gap-[30px]  py-[20px]">
-      <Nav />
-      <Hero />
-      <BookList />
+    <div style={{backgroundImage:`url('${bg}')`}} className="h-full w-full bg-cover bg-no-repeat">
+      <div className="flex flex-col gap-[30px]  py-[20px]">
+        <Nav />
+        <Hero />
+        <BookList />
+      </div>
     </div>
-    </div>
- 
   );
 }
