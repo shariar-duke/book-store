2d8037816261689fae78b1a9eb1f751ffe4d4d15
shariar-duke <shariarmahmud225@gmail.com>
2024-06-23 08:49:08 +0600

    lifting state up to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import Nav from "./components/Nav";
 import Hero from "./components/Hero";
 import BookList from "./components/BookList";
 import bg from "./assets/bookBackground.jpg"
+import { getBookList } from "./data/book-data";
+import { useState } from "react";
 export default function App() {
+  const books = getBookList();
+  const [bookList, setBookList] = useState(books);
   return (
     <div style={{backgroundImage:`url('${bg}')`}} className="h-full w-full bg-cover bg-no-repeat">
       <div className="flex flex-col gap-[30px]  py-[20px]">
         <Nav />
-        <Hero />
-        <BookList />
+        <Hero setBookList={setBookList} bookList={bookList} />
+        <BookList bookList={bookList} />
       </div>
     </div>
   );
