96e6249881411a5a4452169fac4575a2cbd9b3c7
shariar-duke <shariarmahmud225@gmail.com>
2024-06-23 07:56:34 +0600

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import Nav from "./components/Nav";
+import Hero from "./components/Hero";
+import BookList from "./components/BookList";
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div>
+   <div className="flex flex-col gap-[30px]  py-[20px]">
+      <Nav />
+      <Hero />
+      <BookList />
+    </div>
+    </div>
+ 
+  );
 }
