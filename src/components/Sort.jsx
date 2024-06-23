e4bc4836c5c1295edd2ce0eedb5d78d117dafe36
shariar-duke <shariarmahmud225@gmail.com>
2024-06-23 07:46:16 +0600

    filter component

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
 
 export default function Sort() {
   return (
-    <div>Sort</div>
-  )
+    <div className="flex items-stretch space-x-3">
+      <select
+        className="cursor-pointer rounded-md border px-4 py-2 text-center text-gray-600"
+        name="sortBy"
+        id="sortBy"
+      >
+        <option value="">Sort</option>
+        <option value="name_asc">Name (A-Z)</option>
+        <option value="name_desc">Name (Z-A)</option>
+        <option value="year_asc">Publication Year (Oldest)</option>
+        <option value="year_desc">Publication Year (Newest)</option>
+      </select>
+    </div>
+  );
 }
